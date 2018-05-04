bd88703302158352de4e027437916f442ad2b392
atian <a.tian@live.cn>
2018-05-04 20:15:18 +0800

    a

---
+++ struct/vectorfile.c
@@ -0,0 +1,25 @@
+/*
+ * vector.c
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "vector.h"
+
+Vector Vector_construct(int a, int b, int c)
+{
+	Vector v;
+	v.x = a;
+	v.y = b;
+	v.z = c;
+	return v;
+}
+
+vodi Vector_print(char * name, Vector v)
+{
+	printf(""
+int main(int argc, char *argv[])
+{
+	printf("ahahah");
+	return EXIT_SUCCESS;
+}

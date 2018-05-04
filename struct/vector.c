4eb68243471c68a8838be14e94a3e8d2c6fb3939
atian <a.tian@live.cn>
2018-05-04 19:51:39 +0800

    begin struct part

---
+++ struct/vector.c
@@ -0,0 +1,13 @@
+/*
+ * vector.c
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "vector.h"
+
+int main(int argc, char *argv[])
+{
+	printf("ahahah");
+	return EXIT_SUCCESS;
+}

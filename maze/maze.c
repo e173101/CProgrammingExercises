21160df62cadb1134c43b45c6c2769089316a5f8
atian <a.tian@live.cn>
2018-05-07 10:37:52 +0800

    finish maze.c

--- maze/maze.c
+++ maze/maze.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 	int col =0;
 	int numberBrick =0;
 	int exitRow, exitCol;
-	int startRowm ,startCol;
+	int startRow ,startCol;
 	int numberCol, numberRow;
 	if (argc < 2)
 	{
@@ -31,18 +31,24 @@ int main(int argc, char *argv[])
 		switch(ch)
 		{
 			case '*': numberBrick ++;break;
+			case 'S': startRow = row; startCol = col; break;
+			case 'E': exitRow = row; exitCol = col;break;
 			default: break;
 		}
 		switch(ch)
 		{
 			default: col ++;break;
-			case '\n': row ++; col=0;break;
+			case '\n': row ++; numberCol = col;col=0;break;
 			case EOF:break;
 		}
 	}while(ch != EOF);
 	fclose(fptr);
-	numberCol = col;
 	numberRow = row;
 	printf("The maze has %d bricks.\n",numberBrick);
+	printf("The maze has %d rows.\n",numberRow);
+	printf("The maze has %d cols.\n",numberCol);
+	printf("The starting location is at (%d %d) .\n",startRow, startCol);
+	printf("The exit is at (%d %d) .\n",exitRow, exitCol);
+	printf("The maze has %d cols.\n",numberCol);
 	return EXIT_SUCCESS;
 }
